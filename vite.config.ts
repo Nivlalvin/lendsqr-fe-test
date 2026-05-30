2c244032d5bca321dbc056ee1519779297c4346e
Nivlalvin <saintnivla@gmail.com>
2026-05-30 15:51:13 +0300

    update2

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/tests/setup.ts',
-    css: false,
-    testTimeout: 30000,
-  },
 });
